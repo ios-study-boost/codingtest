5104e2bbb28beca59d2054319eb17cdc7075e828
lji <alslahdk@gmail.com>
2020-09-03 07:54:58 +0900

    @jungin : 0901 codingtest

--- 이정인/0903.py
+++ 이정인/0903.py
@@ -1,8 +1,5 @@
 def solution(array, commands):
     answer = []
-    temp = []
     for c in commands:
-        temp = array[c[0]-1:c[1]]
-        temp.sort()
-        answer.append(temp[c[2]-1])
+        answer.append(sorted(array[c[0]-1:c[1]])[c[2]-1])
     return answer
