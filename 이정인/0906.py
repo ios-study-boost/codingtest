600ac99b2308e70c33da659f4a6a9cdb32b59757
lji <alslahdk@gmail.com>
2020-09-07 08:07:04 +0900

    @Minimoa - 0901 타켓넘버

---
+++ 이정인/0906.py
@@ -0,0 +1,10 @@
+def solution(numbers, target):
+    q = [0]
+    for n in numbers:
+        s = []
+        for _ in range(len(q)):
+            x = q.pop()
+            s.append(x + n)
+            s.append(x + n*(-1))
+        q = s.copy()
+    return q.count(target)
